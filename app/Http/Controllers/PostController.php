7f1052b6b4b7bbcdedd2806ab30935223af5e2e5
Quang Nguyen <quang.nguyen@mgm-tp.com>
2018-07-29 15:56:16 +0700

    Forms: Setting up files for Post management

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class StudentsController extends Controller
+class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -29,8 +29,7 @@ class StudentsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request $request
-     *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -41,8 +40,7 @@ class StudentsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int $id
-     *
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -53,8 +51,7 @@ class StudentsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int $id
-     *
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -65,9 +62,8 @@ class StudentsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request $request
-     * @param  int $id
-     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -78,26 +74,11 @@ class StudentsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int $id
-     *
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         //
     }
-
-    public function contact()
-    {
-        $people = ['Quang', 'Ba', 'Nhat', 'Nguyen'];
-
-        return view('students.contact', compact('people'));
-    }
-
-    public function contactStudent($name, $param1, $param2)
-    {
-        return view('students.contact-student', compact('name', 'param1', 'param2'));
-    }
-
-
 }
---
+++ routes/web-demo.php
@@ -0,0 +1,186 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Address;
+use App\Country;
+use App\Photo;
+use App\Post;
+use App\Role;
+use App\Students;
+use App\Tag;
+use App\User;
+use App\Video;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('students', 'StudentsController');
+
+Route::get('/contact', 'StudentsController@contact');
+
+Route::get('/contact/{name}/{param1}/{param2}', 'StudentsController@contactStudent');
+
+Route::get('/students', function () {
+
+    $students = Students::all();
+
+    return $students;
+});
+
+Route::get('/demo-create', function () {
+    $students = new Students;
+    $students->f_name = "quang";
+    $students->l_name = "nguyen";
+
+    $students->save();
+});
+
+Route::get('/demo-update', function () {
+    Students::where('id', 1)->update(['f_name'=> 'quang-update']);
+});
+
+Route::get('/demo-delete', function () {
+    $student = Students::find(1);
+    $student->delete();
+});
+
+Route::get('/soft-delete', function () {
+    $student = Students::find(3);
+    $student->delete();
+});
+
+Route::get('/read-soft-delete', function () {
+    $student = Students::withTrashed()->get();
+    return $student;
+});
+
+Route::get('/show-only-trashed', function () {
+    $student = Students::onlyTrashed()->get();
+    return $student;
+});
+
+Route::get('/restore-trashed', function () {
+    $student = Students::where('id', 3)->restore();
+    return $student;
+});
+
+Route::get('/force-delete', function () {
+    $student = Students::where('id', 3)->forceDelete();
+    return $student;
+});
+
+Route::get('/users/{id}/post', function ($id) {
+    return User::find($id)->post;
+});
+
+Route::get('/posts/{id}/user', function ($id) {
+    return Post::find($id)->user;
+});
+
+Route::get('/users/{id}/posts', function ($id) {
+    $posts = User::find($id)->posts;
+    foreach ($posts as $post) {
+        echo $post->title . "<br>";
+    }
+});
+
+Route::get('/users/{id}/roles', function ($id) {
+    $roles = User::find($id)->roles;
+    return $roles;
+});
+
+Route::get('/roles/{id}/users', function ($id) {
+    $users = Role::find($id)->users;
+    return $users;
+});
+
+Route::get('/users/{id}/pivot', function ($id) {
+    $roles = User::find($id)->roles;
+
+    foreach ($roles as $role) {
+        echo $role->pivot;
+    }
+});
+
+Route::get('/country/{id}/post', function ($id) {
+    $posts = Country::find($id)->posts;
+    foreach ($posts as $post) {
+        echo $post->title;
+    }
+});
+
+Route::get('/user/{id}/photo', function ($id) {
+    $photos = User::find($id)->photos;
+
+    foreach ($photos as $photo) {
+        echo $photo->path . "<br/>";
+    }
+});
+
+Route::get('/post/{id}/photo', function ($id) {
+    $photos = Post::find($id)->photos;
+
+    foreach ($photos as $photo) {
+        echo $photo->path . "<br/>";
+    }
+});
+
+Route::get('/photo/{id}/type', function ($id) {
+    $imageable = Photo::find($id);
+
+    echo $imageable->imageable_type. "<br/>";
+    echo $imageable;
+});
+
+Route::get('/post/{id}/tag', function ($id) {
+    $tags = Post::find($id)->tags;
+    foreach ($tags as $tag) {
+        echo $tag->name . "<br/>";
+    }
+});
+
+Route::get('/video/{id}/tag', function ($id) {
+    $tags = Video::find($id)->tags;
+    foreach ($tags as $tag) {
+        echo $tag->name . "<br/>";
+    }
+});
+
+Route::get('/tag/{id}/video', function ($id) {
+    $tags = Tag::find($id);
+    foreach ($tags->videos as $video) {
+        echo $video->name . "<br/>";
+    }
+});
+
+Route::get('/demo-create-address', function () {
+    $user = User::find(1);
+    $address = new Address(['address' => 'Ha Noi']);
+    $user->address()->save($address);
+});
+
+Route::get('/demo-update-address', function () {
+    $address = Address::whereUserId(1)->first();
+    $address->address = "address updated";
+    $address->save();
+});
+
+Route::get('/check-role', function () {
+    $user = User::find(1);
+    if ($user->has('roles')) {
+        foreach ($user->roles as $role) {
+            echo $role->name. "<br/>";
+        }
+    }
+});
