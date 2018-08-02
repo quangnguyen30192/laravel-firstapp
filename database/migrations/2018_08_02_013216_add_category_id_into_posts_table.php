c47e7853767ec3fed3ef5bd45b5554da0935508d
Quang Nguyen <quang.nguyen@mgm-tp.com>
2018-08-02 08:40:20 +0700

    Application: Posts: create migrations for categories and posts table

--- database/migrations/2018_08_02_013216_add_category_id_into_posts_table.php
+++ database/migrations/2018_08_02_013216_add_category_id_into_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPostTable extends Migration
+class AddPhotoIdCategoryIdIntoPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class AddPostTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->tinyInteger('user_id')->unsigned()->index();
-            $table->string('title');
-            $table->text('content');
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+           $table->integer('category_id')->unsigned()->index();
         });
     }
 
@@ -29,6 +25,8 @@ class AddPostTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('category_id');
+        });
     }
 }
--- database/migrations/2018_08_02_013827_create_categories_table.php
+++ database/migrations/2018_08_02_013827_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPostTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class AddPostTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->tinyInteger('user_id')->unsigned()->index();
-            $table->string('title');
-            $table->text('content');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class AddPostTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropCo('categories');
     }
 }
