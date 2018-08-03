af277c4f1011cd941942907974b3dc068a4a495d
Quang Nguyen <quang.nguyen@mgm-tp.com>
2018-08-04 01:55:23 +0700

    Application: Post -> show with logged in user

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CreatePostRequest;
 use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -15,7 +16,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::latestQuangPost();
+        $posts = Post::whereId(Auth::user()->id);
         return view('posts.index', compact('posts'));
     }
 
