f9ea1a31f0f15e7b8eecb8d489646b76703db48a
Quang Nguyen <quang.nguyen@mgm-tp.com>
2018-08-04 19:21:20 +0700

    fix font-awesome - css fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,7 +13,9 @@
     <title>Admin</title>
 
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
-@yield('style')
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+    @yield('style')
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 
